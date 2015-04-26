fa4dfe43879a56b81c4ec26138cad97d7fceb9f6
Desire Aheza <aheza007@gmail.com>
2015-04-26 09:15:50 -0500

    Commit of Added Routes, Added controllers and Admin Area

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,15 +10,19 @@ namespace SimpleBlog
 {
     public class RouteConfig
     {
+       
         public static void RegisterRoutes(RouteCollection routes)
         {
+            var namespaces = new[] { typeof(PostsController).Namespace };
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+            routes.MapRoute("Home", "", new { Controller="Posts",Action="Index"},namespaces);
+            routes.MapRoute("Login", "login", new { Controller = "Auth", Action = "Login" },namespaces);
         }
     }
 }
